7e437452a327cb3047a6fecd72adce25882af854
Сергей Задорин <szadorin@htc-cs.ru>
2021-06-04 18:56:03 +0400

    Пробую сделать serverless

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-const fastifyCors = require('fastify-cors');
-const fastify = require('fastify')({
-    logger: true
-});
-
-fastify.register(fastifyCors, {
-    origin: "*",
-    methods: ["POST", "GET", "OPTIONS"]
-});
-
-fastify.get('/api/app', async (request: any, reply: any) => {
-    reply.type('application/json').code(200);
-    return { body: { hello: 'world' } }
-});
-
-fastify.listen(8080, (err: Error, address: string) => {
-    if (err) throw err;
-    // Server is now listening on ${address}
-})
+// const fastifyCors = require('fastify-cors');
+// const fastify = require('fastify')({
+//     logger: true
+// });
+//
+// fastify.register(fastifyCors, {
+//     origin: "*",
+//     methods: ["POST", "GET", "OPTIONS"]
+// });
+//
+// fastify.get('/api/app', async (request: any, reply: any) => {
+//     reply.type('application/json').code(200);
+//     return { body: { hello: 'world' } }
+// });
+//
+// fastify.listen(8080, (err: Error, address: string) => {
+//     if (err) throw err;
+//     // Server is now listening on ${address}
+// })
+module.exports = (req, res) => {
+    const { name = 'World' } = req.query;
+    res.status(200).send(`Hello ${name}!`);
+};
